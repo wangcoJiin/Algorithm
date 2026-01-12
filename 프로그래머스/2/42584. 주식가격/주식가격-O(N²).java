64b156de28698c246c669f8e7c03004d7037a089
wangcoJiin <mf0d070208@gmail.com>
2026-01-13 02:46:04 +0900

    O(N^2)으로 우선 풀어봤던 방식 추가
    Implement O(N^2) solution for stock prices problem.

--- 프로그래머스/2/42584. 주식가격/주식가격-O(N²).java
+++ 프로그래머스/2/42584. 주식가격/주식가격-O(N²).java
@@ -1,1 +1,22 @@
+/* O(N^2) 로 풀어보기 */
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
 
+        // O(N^2) 로 풀어보기
+        for (int i = 0 ; i < prices.length ; i ++) {
+            int count = 0;
+            for (int j = (i + 1) ; j < prices.length ; j ++ ) {
+                if (prices[j] >= prices[i] && count == 0) {
+                    answer[i] += 1;
+                }
+                else if (count == 0 && prices[j] < prices[i]) {
+                    answer[i] += 1;
+                    count ++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
